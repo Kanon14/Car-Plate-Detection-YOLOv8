fdc440fa8dac7fce9c050dfc4b602b9647c99968
Kanon14 <geochua144@gmail.com>
2024-05-25 16:39:41 +0800

    step 5 clean license plate format done

--- main.py
+++ main.py
@@ -11,22 +11,123 @@ from ultralytics import YOLO
 coco_model = YOLO("./yolov8n_train/yolov8n.pt") # For cars
 np_model = YOLO("./yolov8n_train/best.pt") # For car plate
 
+# Initialize the OCR reader
+reader = easyocr.Reader(['en'], gpu=True)
+
+# Mapping dictionaries for character conversion
+# characters that can easily be confused can be 
+# verified by their location - an `O` in a place
+# where a number is expected is probably a `0`
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
+def write_csv(results, output_path):
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{},{},{},{},{}\n'.format(
+            'frame_number', 'track_id', 'car_bbox', 'car_bbox_score',
+            'license_plate_bbox', 'license_plate_bbox_score', 'license_plate_number',
+            'license_text_score'))
+
+        for frame_number in results.keys():
+            for track_id in results[frame_number].keys():
+                print(results[frame_number][track_id])
+                if 'car' in results[frame_number][track_id].keys() and \
+                   'license_plate' in results[frame_number][track_id].keys() and \
+                   'number' in results[frame_number][track_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{},{}\n'.format(
+                        frame_number,
+                        track_id,
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['car']['bbox'][0],
+                            results[frame_number][track_id]['car']['bbox'][1],
+                            results[frame_number][track_id]['car']['bbox'][2],
+                            results[frame_number][track_id]['car']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['car']['bbox_score'],
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['license_plate']['bbox'][0],
+                            results[frame_number][track_id]['license_plate']['bbox'][1],
+                            results[frame_number][track_id]['license_plate']['bbox'][2],
+                            results[frame_number][track_id]['license_plate']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['license_plate']['bbox_score'],
+                        results[frame_number][track_id]['license_plate']['number'],
+                        results[frame_number][track_id]['license_plate']['text_score'])
+                    )
+        f.close()
+
+def license_complies_format(text):
+    # True if the license plate complies with the format, False otherwise.
+    if len(text) != 7:
+        return False
+
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+        return True
+    else:
+        return False
+
+def format_license(text):
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
+               2: dict_char_to_int, 3: dict_char_to_int}
+    for j in [0, 1, 2, 3, 4, 5, 6]:
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
+
+def read_license_plate(license_plate_crop):
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+
+        # verify that text is conform to a standard license plate
+        if license_complies_format(text):
+            # bring text into the default license plate format
+            return format_license(text), score
+
+    return None, None
+
 # Read in test video paths
 videos = glob("./test_sample/*.mp4")
 
-# read video by index
-video = cv.VideoCapture(videos[3])
+results = {}
+
+# Read video by index
+video = cv.VideoCapture(videos[0])
 
 ret = True
 frame_number = -1
 vehicles = [2,3,5]
 
-# read the 10 first frames
+# read the entire video
 while ret:
-    frame_number += 1
     ret, frame = video.read()
-
-    if ret and frame_number < 10:
+    frame_number += 1
+    if ret: # Testing with 100 frames first
+        results[frame_number] = {}
         
         # vehicle detector
         detections = coco_model.track(frame, persist=True)[0]
@@ -38,18 +139,41 @@ while ret:
                 for bbox in vehicle_bounding_boxes:
                     print(bbox)
                     roi = frame[int(y1):int(y2), int(x1):int(x2)]
-                    # debugging check if bbox lines up with detected vehicles (should be identical to save_crops() above
-                    # cv.imwrite(str(track_id) + '.jpg', roi)
                     
                     # license plate detector for region of interest
                     license_plates = np_model(roi)[0]
-                    # check every bounding box for a license plate
+                    # process license plate
                     for license_plate in license_plates.boxes.data.tolist():
                         plate_x1, plate_y1, plate_x2, plate_y2, plate_score, _ = license_plate
-                        # verify detections
-                        print(license_plate, 'track_id: ' + str(bbox[4]))
+                        # crop plate from region of interest
                         plate = roi[int(plate_y1):int(plate_y2), int(plate_x1):int(plate_x2)]
-                        cv.imwrite(str(track_id) + '.jpg', plate)
+                        # de-colorize
+                        plate_gray = cv.cvtColor(plate, cv.COLOR_BGR2GRAY)
+                        # posterize
+                        _, plate_treshold = cv.threshold(plate_gray, 64, 255, cv.THRESH_BINARY_INV)
                         
+                        # OCR
+                        np_text, np_score = read_license_plate(plate_treshold)
+                        # if plate could be read write results
+                        if np_text is not None:
+                            results[frame_number][track_id] = {
+                                'car': {
+                                    'bbox': [x1, y1, x2, y2],
+                                    'bbox_score': score
+                                },
+                                'license_plate': {
+                                    'bbox': [plate_x1, plate_y1, plate_x2, plate_y2],
+                                    'bbox_score': plate_score,
+                                    'number': np_text,
+                                    'text_score': np_score
+                                }
+                            }
+
+write_csv(results, './results.csv')
 video.release()
+
+results = pd.read_csv('./results.csv')
+
+# show results for tracking ID `1` - sort by OCR prediction confidence
+results[results['track_id'] == 1.].sort_values(by='license_text_score', ascending=False)
                 
