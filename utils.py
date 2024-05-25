8d2abc35926fe31e67d09f7791a802b94363b7d9
Kanon14 <geochua144@gmail.com>
2024-05-25 18:07:57 +0800

    utils added

---
+++ utils.py
@@ -0,0 +1,106 @@
+import cv2 as cv
+import easyocr
+import numpy as np
+import string
+
+# Initialize the OCR reader
+reader = easyocr.Reader(['en'], gpu=True)
+
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0', 'I': '1', 'J': '3', 'A': '4', 'G': '6', 'S': '5'}
+dict_int_to_char = {'0': 'O', '1': 'I', '3': 'J', '4': 'A', '6': 'G', '5': 'S'}
+
+def write_csv(results, output_path):
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{},{},{},{},{}\n'.format(
+            'frame_number', 'track_id', 'car_bbox', 'car_bbox_score',
+            'license_plate_bbox', 'license_plate_bbox_score', 'license_plate_number',
+            'license_text_score'))
+
+        for frame_number in results.keys():
+            for track_id in results[frame_number].keys():
+                if 'car' in results[frame_number][track_id].keys() and \
+                   'license_plate' in results[frame_number][track_id].keys() and \
+                   'number' in results[frame_number][track_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{},{}\n'.format(
+                        frame_number,
+                        track_id,
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['car']['bbox'][0],
+                            results[frame_number][track_id]['car']['bbox'][1],
+                            results[frame_number][track_id]['car']['bbox'][2],
+                            results[frame_number][track_id]['car']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['car']['bbox_score'],
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['license_plate']['bbox'][0],
+                            results[frame_number][track_id]['license_plate']['bbox'][1],
+                            results[frame_number][track_id]['license_plate']['bbox'][2],
+                            results[frame_number][track_id]['license_plate']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['license_plate']['bbox_score'],
+                        results[frame_number][track_id]['license_plate']['number'],
+                        results[frame_number][track_id]['license_plate']['text_score'])
+                    )
+        f.close()
+
+def license_complies_format(text):
+    # True if the license plate complies with the format, False otherwise.
+    if len(text) != 7:
+        return False
+
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+        return True
+    else:
+        return False
+
+def format_license(text):
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
+               2: dict_char_to_int, 3: dict_char_to_int}
+    for j in range(7):
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
+
+def read_license_plate(license_plate_crop):
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+
+        # verify that text is conform to a standard license plate
+        if license_complies_format(text):
+            # bring text into the default license plate format
+            return format_license(text), score
+
+    return None, None
+
+def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=6, line_length_x=200, line_length_y=200):
+    x1, y1 = top_left
+    x2, y2 = bottom_right
+
+    cv.line(img, (x1, y1), (x1, y1 + line_length_y), color, thickness)  #-- top-left
+    cv.line(img, (x1, y1), (x1 + line_length_x, y1), color, thickness)
+
+    cv.line(img, (x1, y2), (x1, y2 - line_length_y), color, thickness)  #-- bottom-left
+    cv.line(img, (x1, y2), (x1 + line_length_x, y2), color, thickness)
+
+    cv.line(img, (x2, y1), (x2 - line_length_x, y1), color, thickness)  #-- top-right
+    cv.line(img, (x2, y1), (x2, y1 + line_length_y), color, thickness)
+
+    cv.line(img, (x2, y2), (x2, y2 - line_length_y), color, thickness)  #-- bottom-right
+    cv.line(img, (x2, y2), (x2 - line_length_x, y2), color, thickness)
+
+    return img
